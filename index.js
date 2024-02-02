49121b802f3bf081658154f61cb599b92f6d2499
Alexey <alezgora@gmail.com>
2024-02-02 18:41:12 +0200

    Revert "Merge pull request #100 from fm1randa/link-with-phone-number"
    This reverts commit 55f6c411ab07fe40eff04767601b2dbfd6593c0a, reversing
    changes made to 66591008c6ddba08eea239b538bbb556bd1ba92f.

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ module.exports = {
     ProductMetadata: require('./src/structures/ProductMetadata'),
     List: require('./src/structures/List'),
     Buttons: require('./src/structures/Buttons'),
-    LinkingMethod: require('./src/LinkingMethod'),
     
     // Auth Strategies
     NoAuth: require('./src/authStrategies/NoAuth'),
